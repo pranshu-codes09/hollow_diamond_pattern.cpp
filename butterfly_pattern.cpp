b1b136ecf714e858db68c5e1ae6bae726cbc91f9
pranshu-codes09 <pranshu7vachkal@gmail.com>
2026-06-26 17:25:13 +0530

    Update butterfly_pattern.cpp

--- butterfly_pattern.cpp
+++ butterfly_pattern.cpp
@@ -1,1 +1,30 @@
-butterfly pattern in cpp
+#include <iostream>
+using namespace std;
+int main() {
+    int n = 5;
+    for (int i = 0; i<n;i++){
+        for(int j = 0;j<=i;j++){
+            cout<<"*";
+        }
+        for(int j = 0;j<(n-i-1);j++){
+            cout<<"  ";
+        }
+        for(int j = 0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for (int i=n-1; i>=0;i--){
+        for(int j = 0;j<=i;j++){
+            cout<<"*";
+        }
+        for(int j = 0;j<(n-i-1);j++){
+            cout<<"  ";
+        }
+        for(int j = 0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
