81cb11b94c58b492e8ff99791e1faadcd219e426
pranshu-codes09 <pranshu7vachkal@gmail.com>
2026-06-26 17:26:24 +0530

    Update butterfly_pattern.cpp
    Added comments

--- butterfly_pattern.cpp
+++ butterfly_pattern.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 int main() {
     int n = 5;
+
+//for upper part
     for (int i = 0; i<n;i++){
         for(int j = 0;j<=i;j++){
             cout<<"*";
@@ -13,7 +15,10 @@ int main() {
             cout<<"*";
         }
         cout<<endl;
-    }
+        }
+
+//for lower part 
+    
     for (int i=n-1; i>=0;i--){
         for(int j = 0;j<=i;j++){
             cout<<"*";
